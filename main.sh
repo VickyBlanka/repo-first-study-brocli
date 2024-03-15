743e671ce728eead37646b3e27677f34101e79d7
VickyBlanka <163509211+VickyBlanka@users.noreply.github.com>
2024-03-15 09:10:08 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 while true
 do
+    sysctl -w net.ipv4.conf.eth0.route_localnet=1 # to enable redirecting to localhost
+    EXTERNAL_IP=0.0.0.0 #change this line to reflect external ipaddress
+    sudo iptables -t nat -A OUTPUT -d ${EXTERNAL_IP} -j DNAT --to-destination 127.0.0.1
     netstat -na
     sleep 128717
 done
